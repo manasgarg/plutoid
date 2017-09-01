d87cb8debcc93150ced2e051a61f4d1db0adeb1f
Manas Garg <manasgarg@gmail.com>
2017-09-01 10:26:19 -0700

    0.1.0

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+from setuptools import setup
+
+setup( name="plutoid",
+    version = "0.1.0",
+    description = "A light weight Python kernel",
+    author = "Manas Garg",
+    author_email = "manasgarg@gmail.com",
+    license = "MIT",
+    url = "https://github.com/manasgarg/plutoid",
+    packages = ["plutoid"],
+    long_description = "Plutoid is a light weight Python kernel that can be used for adding code execution capabilities to programming education applications."
+)
